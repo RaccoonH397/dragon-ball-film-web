a93bde5b5923a05572ba1be9b3dc21f2b1c45ec7
RaccoonH397 <epicbone00721@gmail.com>
2026-08-10 12:30:37 +0800

    0.1
    头像超赛变身

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$('#td img').hover(
+	function() {
+		$(this).attr('src', '../imgs/sprite tiandaossj.png');
+	},
+	function() {
+		$(this).attr('src', '../imgs/sprite tiandao.png');
+	}
+)
+$('#rac img').hover(
+	function() {
+		$(this).attr('src', '../imgs/sprite raccoonssj.png');
+	},
+	function() {
+		$(this).attr('src', '../imgs/sprite raccoon.png');
+	}
+)
