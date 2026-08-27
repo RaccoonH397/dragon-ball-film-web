4af9b49b2f81109cfce3ce0e9bda6fbbe39b2b64
RaccoonH397 <epicbone00721@gmail.com>
2026-08-27 18:02:43 +0800

    0.1.7
    新增了人物档案详情页面

--- js/chardata - 副本.js
+++ js/chardata - 副本.js
@@ -3,389 +3,389 @@ const data = [{
 		film: '影片00',
 		charname: '悟吉塔',
 		chardetails: '为打败失控的布罗利，悟空与贝吉塔不得已融合成的究极战士，实力之强，甚至压制了失控的布罗利。在时空裂缝与其对拼力量之时，激烈的力量碰撞导致了黑洞的出现...',
-		saga: '龙珠超-布罗利篇。',
-		appraise: '不愧是无敌的次元最强大人啊！',
-		situation: '压制着布罗利。',
-		feelings: '与强敌激战的兴奋。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/00/broly00/broly00.png',
 		film: '影片00',
 		charname: '布罗利',
 		chardetails: '因帕拉卡斯的死，导致布罗利失控变身成了超级赛亚人，在悟吉塔的压制下，他再次突破极限爆发出了惊人的力量！在时空裂缝与其对拼力量之时，激烈的力量碰撞导致了黑洞的出现...',
-		saga: '龙珠超-布罗利篇',
-		appraise: '哥们你也太血厚了些。',
-		situation: '被悟吉塔压制着。',
-		feelings: '开始有点害怕了。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/01/goku01/goku01.png',
 		film: '影片01',
 		charname: '孙悟空',
 		chardetails: '在“神与神”时期过后，悟空与贝吉塔通过“贿赂”维斯而得以在比鲁斯星修炼。可惜，“超级赛亚人之神”可不是他们所想的那么简单就能掌握的...',
-		saga: '龙珠Z-神与神篇。',
-		appraise: '神境界什么的，感觉还是超级赛亚人三帅啊。',
-		situation: '与贝吉塔对练。',
-		feelings: '对神境界的期待。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/01/vegeta01/vegeta01.png',
 		film: '影片01',
 		charname: '贝吉塔',
 		chardetails: '在“神与神”时期过后，贝吉塔和孙悟空通过“贿赂”维斯而得以在比鲁斯星修炼。可惜，“超级赛亚人之神”可不是他们所想的那么简单就能掌握的...',
-		saga: '龙珠Z-神与神篇。',
-		appraise: '要什么超级赛亚人三，要学就直接学更强的！',
-		situation: '与卡卡罗特对练。',
-		feelings: '迫切想超越卡卡罗特。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/01/beerus01/beerus01.png',
 		film: '影片01',
 		charname: '比鲁斯',
 		chardetails: '原本想美美睡一觉，却被外面的动静吵醒，没想到竟是这两个赛亚人扰他美梦...！',
-		saga: '龙珠Z-神与神篇。',
-		appraise: '臭小子们可知我打个喷嚏就能破坏你们！',
-		situation: '打爆孙悟空和贝吉塔。',
-		feelings: '恼怒。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/02/android17 super 02/android 17 02.png',
 		film: '影片02',
 		charname: '人造人17号（超）',
 		chardetails: '受黑洞的影响，17号来到了一个陌生的时空。未曾想，迷茫之余，他看到了一个奇怪的装置...',
-		saga: '龙珠超-布罗利篇。',
-		appraise: '人造神降临炸鱼!',
-		situation: '击败未来世界的敌人。',
-		feelings: '对未来世界所发生的事情感到愤怒。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/02/trunks02/trunks02.png',
 		film: '影片02',
 		charname: '特兰克斯（未来）',
 		chardetails: '在孙悟饭死后，特兰克斯独自修行，自以为能够打败人造人，却惨败于他们之手。在恢复伤势之后，决定听从布尔玛建议，使用时光机回到未来，但就在他准备出发之时，人造人17号却出现在了时光机面前...',
-		saga: '龙珠Z-未来特兰克斯所在的时空。',
-		appraise: '若悟饭能够多活几年，说不定与现在的特兰克斯能够一起击败人造人们...',
-		situation: '保护时光机，击败未来世界的敌人。',
-		feelings: '愤怒、悲伤。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/02/gohan02/gohan02.png',
 		film: '影片02',
 		charname: '孙悟饭（未来）',
 		chardetails: '早在几年前孙悟饭被人造人杀死之后，他开始在地府努力训练，并同时向阎王求情，希望能够回到地球一天帮助特兰克斯铲除人造人。但因阎王不愿破坏人间的规则，而一直未能如愿。终于，阎王见证了特兰克斯的觉悟，决定让悟饭踏上讨伐的征途...',
-		saga: '龙珠Z-未来特兰克斯所在的时空。',
-		appraise: '让人造人体会被所杀之人的愤怒吧！',
-		situation: '支援特兰克斯，拯救未来！',
-		feelings: '对特兰克斯的欣慰。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/02/futrue 17 18 02/171802.png',
 		film: '影片02',
-		charname: '人造人17和18号（未来）',
+		charname: '人造人17 18号（未来）',
 		chardetails: '在格罗博士激活了这对姐弟之后，他们开始惨无人道地屠杀人类，将其视为一场场游戏，肆意玩弄人类的生命。可其不知道的是，他们很快将会遭到应有的惩罚...',
-		saga: '龙珠Z-未来特兰克斯所在的时空。',
-		appraise: '这两货简直就是畜生。',
-		situation: '与正义之士激战。',
-		feelings: '开始对对手的决心和力量感到害怕。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/02/cell 02/cell021.png',
 		film: '影片02',
 		charname: '沙鲁（第一阶段）',
-		chardetails: '因力量不及人造人17号和18号，沙鲁一直在寻找机会吸收二人。发现阴谋无法得逞后，他只得乘坐时光机回到过去再次寻找机会，却没想到另一个人造人17号先行发现了他...',
-		saga: '龙珠Z-未来特兰克斯所在的时空。',
-		appraise: '不是哥们这个17号实力怎么这么离谱？ ！',
-		situation: '被另一个人造人17号打得头破血流。',
-		feelings: '恐惧。'
+		chardetails: '因力量不及人造人17号和18号，沙鲁一直在寻找机会吸收二人。发现阴谋无法得逞后，他只得乘坐时光机回到过去再次寻找机会，却没想到另一个17号先行发现了他...',
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/03/bardock03/bardock03.png',
 		film: '影片03',
 		charname: '巴达克',
 		chardetails: '数十年前，巴达克在弗利萨的灭星一击之中穿越到了过去，成为了超级赛亚人并击败了弗利萨的祖先齐尔德。此后，他不断修行，却在途中被神秘的黑洞带走，没想到这一次，他见到了自己的仇人...',
-		saga: '某段未知的过去。',
-		appraise: '再给你一个惊喜，弗利萨。',
-		situation: '观战，并决定痛揍弗利萨，狠狠羞辱一顿。',
-		feelings: '复仇的喜悦。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/03/vegeta03/vegeta03.png',
 		film: '影片03',
 		charname: '贝吉塔',
 		chardetails: '贝吉塔自以为让丹迪治愈之后的濒死回复能让他突破极限，成为自己梦寐以求的超级赛亚人，于是他向弗利萨发起了最后的挑战...',
-		saga: '龙珠Z-那美克星篇。',
-		appraise: '你可是传说的超级赛亚人啊，快上吧！',
-		situation: '自认为超级赛亚人，挑战弗利萨。',
-		feelings: '无知的高兴。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/03/freeza03/freeza03.png',
 		film: '影片03',
 		charname: '弗利萨',
 		chardetails: '长生不老的美梦被众人破坏后，弗利萨开始进行讨伐。变身为最终形态后，面对“超级赛亚人”贝吉塔，他不由得呵呵一笑...',
-		saga: '龙珠Z-那美克星篇。',
-		appraise: '沙包可不是弗利萨大人，而是你啊，贝吉塔！',
-		situation: '羞辱贝吉塔，并遇到了眼熟的神秘赛亚人。',
-		feelings: '拿贝吉塔泄愤，算是愉快的消遣。。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/03/goku03/goku03.png',
 		film: '影片03',
 		charname: '孙悟空',
 		chardetails: '在前面被基纽交换身体之后，孙悟空的肉体不得已受到了很多伤势，只得在治疗舱回复，但是外面各类气息的变化让他感到十分不详...',
-		saga: '龙珠Z-那美克星篇。',
-		appraise: '界王拳不敌，我想你需要名为克林之死的buff。',
-		situation: '为同伴们复仇！',
-		feelings: '对弗利萨的强大感到难以置信。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/04/raditz04/raditz04.png',
 		film: '影片04',
 		charname: '拉蒂兹',
 		chardetails: '为拓展幸存者小队的人数，拉蒂兹来到地球寻找他的弟弟达列斯。令他没想到的是，这趟旅途的收获实在是太大了...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '你的好哥哥即将到达地球。',
-		situation: '寻找自己的弟弟达列斯。',
-		feelings: '对达列斯的实力感到意外。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/04/turles04/turles04.png',
 		film: '影片04',
 		charname: '达列斯',
 		chardetails: '在达列斯杀死了比克大魔王后，彻底成为了地球最强战士！借助红绸军的科技，他的力量得到了显著的提升。而今天，他感应到了一股“熟悉”而又陌生的气息来到了地球...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '这V字头也不像是达列斯大哥的样子啊。',
-		situation: '与自称自己老哥的拉蒂兹开战。',
-		feelings: '对强者来袭的兴奋。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/05/satan05/satan05.png',
 		film: '影片05',
 		charname: '撒旦先生',
 		chardetails: '在沙鲁带来的恐惧笼罩了整个地球之际，人类的救世主——撒旦先生踏上了沙鲁游戏，准备与其决一死战。未曾想，一脚滑却来到了一个陌生的地方...',
-		saga: '龙珠Z-人造人篇。',
-		appraise: '这一脚滑可不得了。',
-		situation: '穿越时空，误打误撞和小悟空发生战斗。',
-		feelings: '疑惑，莫名其妙。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/05/gokukid05/gokukid05.png',
 		film: '影片05',
 		charname: '小悟空',
 		chardetails: '在小悟空安排好午餐后准备回家之际，突然出现的“山羊怪”将他的计划彻底打乱...',
-		saga: '龙珠-无印篇。',
-		appraise: '小时候的悟空太有趣了！',
-		situation: '抓鱼带回家，没想到被山羊怪的出现打乱了计划。',
-		feelings: '生气！'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/05/vegeta05/vegeta05.png',
 		film: '影片05',
 		charname: '贝吉塔',
 		chardetails: '在那美克星见识到了巴达克的超级赛亚人变化后，贝吉塔深知自己还有无限可能。在沙鲁游戏上，他成为了首发成员，准备一雪前耻...',
-		saga: '龙珠Z-人造人篇。',
-		appraise: '在见识到巴达克的超级赛亚人三后，贝吉塔的成长开始发生变化。',
-		situation: '成为首发成员，准备和沙鲁一决高下！',
-		feelings: '对复仇的势在必得。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/05/cell05/cell05.png',
 		film: '影片05',
 		charname: '沙鲁',
 		chardetails: '吸收了人造人姐弟后的沙鲁，得到了究极的力量，为求一战，他举办了名为“沙鲁游戏”的武道会，期待着孙悟空等人的到来...',
-		saga: '龙珠Z-人造人篇。',
-		appraise: '究极缝合怪。',
-		situation: '最期待的孙悟空就应该排后面，先试试前菜吧。',
-		feelings: '对贝吉塔的不知死活感到疑惑。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/06/gohan06/gohan06.png',
 		film: '影片06',
 		charname: '孙悟饭',
 		chardetails: '在沙鲁游戏七年后，悟饭疏于修炼，在学习上发愤图强，导致其暂时丢失了战斗的直觉，身体也发挥不出当年的力量。但布罗利来临之际，他还是毅然决然地代替他的父亲孙悟空应敌...',
-		saga: '龙珠Z-魔人布欧篇前夕。',
-		appraise: '学习使你退步啊悟饭。',
-		situation: '为保护地球挺身而出，迎战布罗利。',
-		feelings: '对自己修炼不足而导致发挥力量的下降敌不过布罗利感到担忧。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/06/vegeta06/vegeta06.png',
 		film: '影片06',
 		charname: '贝吉塔',
 		chardetails: '在沙鲁游戏过后，贝吉塔一直努力地提升自己的实力，终于摸到了那巴达克所变成神秘姿态的门槛，在悟饭危急之际，他挺身而出，势要为过去的自己复仇...',
-		saga: '龙珠Z-魔人布欧篇前夕。',
-		appraise: '变得比原著更加成熟了，你不是贝吉塔，你究竟是谁？！',
-		situation: '营救悟饭，对付布罗利。',
-		feelings: '冷静，而后对布罗利的逆天天赋感到不屑。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/06/broly06/broly06 (1).png',
 		film: '影片06',
 		charname: '布罗利',
 		chardetails: '被悟空击败后，布罗利漂流到了地球，沉睡了七年。苏醒之时，他因濒死恢复而得到了更强的力量。在击败了悟天等人之后，孙悟饭挡在了他的面前...',
-		saga: '龙珠Z-魔人布欧篇前夕。',
-		appraise: '哈哈哈怒了一下就抵过你数年的修炼如何呢？',
-		situation: '努力血祭贝吉塔和孙悟饭。',
-		feelings: '由被贝吉塔压制的愤怒转为愉悦。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/07/bardock07/bardock07.png',
 		film: '影片07',
 		charname: '巴达克',
 		chardetails: '在狠狠地揍了弗利萨一顿后，这次黑洞将他带到了一个陌生的毁灭都市，在那里，他见到了自己的儿子“卡卡罗特”...',
-		saga: '某个未知的过去。',
-		appraise: '教育“儿子”的时候到了。',
-		situation: '为夺取赛亚人儿子躯体的神明展开攻击。',
-		feelings: '对所谓的神明感到愤怒。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/07/1707/1707.png',
 		film: '影片07',
 		charname: '人造人17号（超）',
 		chardetails: '在另一个时空击败了作恶多端的自己以及沙鲁后，黑洞将17号带到了更加遥远的未来，未曾想他竟看到了两个“孙悟空”在战斗...',
-		saga: '龙珠超-布罗利篇。',
-		appraise: '人造神还能够吃鸡吗？',
-		situation: '观察场上情况，决定帮助巴达克。',
-		feelings: '对这个未来的惨况感到愤怒与悲伤。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/07/gokublack07/gokublack07.png',
 		film: '影片07',
 		charname: '黑悟空',
 		chardetails: '与悟空交手后掌握超赛的黑悟空，正想与人交手之际，“孙悟空”竟也来到了这未来...',
-		saga: '龙珠超-未来篇。',
-		appraise: '不要脸之人降临。',
-		situation: '以巴达克作为试金石，尝试自己的完全体超级赛亚人。',
-		feelings: '得到新力量的愉悦。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra01/raditz-extra01/raditz-extra01.png',
 		film: '番外01',
 		charname: '拉蒂兹',
 		chardetails: '得知家乡被弗利萨所毁，幸存者小队发愤图强，并且决定要拓展其队伍。就在这时，拉蒂兹想起了自己远在地球的弟弟——达列斯...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '至少也不再是原著的胆小鬼拉蒂兹了。',
-		situation: '与那巴和贝吉塔一同修炼。',
-		feelings: '对见到弟弟感到期待。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra01/vegeta-extra01/vegeta-extra01.png',
 		film: '番外01',
 		charname: '贝吉塔',
 		chardetails: '作为幸存者小队中的领导者，也是贝吉塔星的王子。为了讨伐弗利萨，贝吉塔决心要把这支队伍的整体实力提升上去，于是他亲自指导那巴与拉蒂兹，也经常去危险的行星进行作战。为让这支小队变得更强，他决定要拓展队伍人数...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '最懂得团结的贝吉塔，很有看点。',
-		situation: '教导那巴和拉蒂兹，考虑拓展人数。',
-		feelings: '对那巴和拉蒂兹的成长感到欣慰。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra01/nappa-extra01/nappa-extra01.png',
 		film: '番外01',
 		charname: '那巴',
 		chardetails: '在贝吉塔得知家乡毁灭的真相后，那巴和拉蒂兹都受到了贝吉塔的“特殊照顾”，因此变得更加强大，对此他对贝吉塔十分感激...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '变成赛亚人好哥们了。',
-		situation: '与拉蒂兹和贝吉塔一同修炼。',
-		feelings: '对拉蒂兹的弟弟感到期待。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra02/gokublack-extra02/gokublack-extra02.png',
 		film: '番外02',
 		charname: '黑悟空',
 		chardetails: '在主时空与孙悟空交手之后，伤势恢复的黑悟空终于也掌握了超级赛亚人的变身，此刻他迫不及待想要有交手的对象，就在这时，“孙悟空”出现在了他的时空...',
-		saga: '龙珠超-未来篇。',
-		appraise: '赛亚人的身体太他妈爽了！',
-		situation: '回到未来，见到了“孙悟空”，',
-		feelings: '为得到新力量而感到愉悦。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra02/bardock-extra02/bardock-extra02.png',
 		film: '番外02',
 		charname: '巴达克',
 		chardetails: '在那美克星痛揍弗利萨一顿后，黑洞又将他带到了一个陌生的时空，在那里，他见到了“卡卡罗特”...',
-		saga: '某个未知的过去。',
-		appraise: '第一次见到长大后的“儿子”居然是在这种情形，有点惨了。',
-		situation: '通过黑洞穿越到陌生的时空。见到了“儿子”。',
-		feelings: '疑惑。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra02/17-extra02/17-extra02.png',
 		film: '番外02',
 		charname: '人造人17号（超）',
 		chardetails: '在打败了另一个未来的自己后，黑洞又将他带到了另一个未来的西都，没想到的是，他看到了两个“孙悟空”在激斗...',
-		saga: '龙珠超-布罗利篇。',
-		appraise: '孙悟空是大众脸吗？',
-		situation: '观察战况。',
-		feelings: '对当前情形感到迷茫。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra03/raditz-extra03/raditz-extra03.png',
 		film: '番外03',
 		charname: '拉蒂兹',
 		chardetails: '与自己的弟弟达列斯交手后，拉蒂兹迫不及待地招呼那巴和贝吉塔到来，欲分享来到地球的巨大收获...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '老弟你说什么？你连我都打不过还要和贝吉塔和那巴打？',
-		situation: '看着弟弟被那巴完全压制。',
-		feelings: '对达列斯的要求感到疑惑。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra03/turles-extra03/turles-extra03.png',
 		film: '番外03',
 		charname: '达列斯',
 		chardetails: '拉蒂兹不吝啬的夸奖，让达列斯心中一阵澎湃，听闻另外两位队友要来，也是跃跃欲试...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '老实说稍微有点得意忘形了。',
-		situation: '被那巴虐爆了。',
-		feelings: '对陌生同族感到兴奋。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra03/vegeta-extra03/vegeta-extra03.png',
 		film: '番外03',
 		charname: '贝吉塔',
 		chardetails: '听闻拉蒂兹说地球有变强秘策，速速与那巴赶到地球，听闻时光屋和重力室，不由得心里一惊...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '想和我打，先过过那巴吧。',
-		situation: '观察达列斯，对他的实力有了一定的了解。',
-		feelings: '对地球的设备和时间屋感到惊讶。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra03/nappa-extra03/nappa-extra03.png',
 		film: '番外03',
 		charname: '那巴',
 		chardetails: '贝吉塔说要去地球，比起变强秘策，更好奇拉蒂兹的弟弟究竟何样...',
-		saga: '龙珠Z-赛亚人来袭篇。',
-		appraise: '不错啊小老弟，打起来比你那老哥豪迈多了。',
-		situation: '对达列斯的战斗风格感到非常喜欢，想收其为徒。',
-		feelings: '非常之喜悦啊。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra04/vegito-extra04/vegito-extra04.png',
 		film: '番外04',
 		charname: '贝吉特',
 		chardetails: '在悟饭布欧袭来之际，贝吉塔不得已与孙悟空进行了耳环合体，最终诞生了史上最强战士贝吉特。在压倒性的力量下，布欧显露了疲态，但未曾想贝吉特的计谋却弄巧成拙...',
-		saga: '龙珠Z-魔人布欧篇。',
-		appraise: '天下无敌的合体爸爸，天塌了都给你怼回去。',
-		situation: '试图激怒布欧让其吸收贝吉特至体内好以营救，没想到弄巧成拙。',
-		feelings: '对布欧的变化感到意外。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/extra04/gohanbuu-extra04/gohanbuu-extra04.png',
 		film: '番外04',
 		charname: '悟饭布欧',
 		chardetails: '作为史上最强的魔人，竟被区区两个赛亚人的合体打的团团转，布欧愤怒之余，比克的智慧却让他发现体内悟饭的未知潜力，加以巴比迪的魔法，他似乎发现了新的大陆...',
-		saga: '龙珠Z-魔人布欧篇。',
-		appraise: '悟饭神力啊朋友们！',
-		situation: '发掘悟饭潜力，成为野兽！',
-		feelings: '由愤怒转为冷静。'
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	},
 	{
 		img: '../imgs/chars/unknown.png',
 		film: '番外04',
 		charname: '110 111 101 001 1000 0100 01 1010 101',
-		chardetails: 'unknown.',
-		saga: 'unkown.',
-		appraise: 'unkown.',
-		situation: 'unkown.',
-		feelings: 'unkown.'
+		chardetails: 'unknown',
+		age: '',
+		appraise: '',
+		situation: '',
+		feelings: ''
 	}
 ];
