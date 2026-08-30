7f32c50fa4252c5c645c92fad910562b126db344
RaccoonH397 <epicbone00721@gmail.com>
2026-08-30 11:26:22 +0800

    2.0.3
    新增了视频可自选跳转平台

--- js/filmdata.js
+++ js/filmdata.js
@@ -1,57 +1,68 @@
 const filmdata = [{
 		filmname: '影片00',
 		cover: '../imgs/cover/00.jpg',
-		url: 'https://www.bilibili.com/video/BV1aL8LzpEGs/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1aL8LzpEGs/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=4WQ_HD77Cqk&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=9&pp=iAQB0gcJCRMMAYcqIYzv'
 	},
 	{
 		filmname: '影片01',
 		cover: '../imgs/cover/01.jpg',
-		url: 'https://www.bilibili.com/video/BV1LxuEzDEEo/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1LxuEzDEEo/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=hpPgtnLDR20&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=8&pp=iAQB'
 	},
 	{
 		filmname: '影片02',
 		cover: '../imgs/cover/02.jpg',
-		url: 'https://www.bilibili.com/video/BV1XfbpzGEZd/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1XfbpzGEZd/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=v5ydBn4r1V8&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=6&pp=iAQB'
 	},
 	{
 		filmname: '影片03',
 		cover: '../imgs/cover/03.png',
-		url: 'https://www.bilibili.com/video/BV14NHJzTEHN/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV14NHJzTEHN/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=k2o3HN7jMnw&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=5&pp=iAQB'
 	},
 	{
 		filmname: '影片04',
 		cover: '../imgs/cover/04.png',
-		url: 'https://www.bilibili.com/video/BV1mv2LBqEW1/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1mv2LBqEW1/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=_aIgRwReQ6g&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=4&pp=iAQB'
 	},
 	{
 		filmname: '影片05',
 		cover: '../imgs/cover/05.png',
-		url: 'https://www.bilibili.com/video/BV18bwWzBEXT/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV18bwWzBEXT/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=cllRbYpdkZY&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=3&pp=iAQB'
 	},
 	{
 		filmname: '影片06',
 		cover: '../imgs/cover/06.png',
-		url: 'https://www.bilibili.com/video/BV1HejB6HEx6/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1HejB6HEx6/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=H7r9YBx2L-0&list=PLmXJJ8Www-ait9QIEA4JiKMmrioP-IVn-&index=1&pp=iAQB'
 	},
 ]
 const extradata = [{
 		filmname: '番外01',
 		cover: '../imgs/cover/extra01.jpg',
-		url: 'https://www.bilibili.com/video/BV1gEyMBEEBB/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1gEyMBEEBB/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=gQvcK9GVmXY&list=PLmXJJ8Www-ajXcF38YnKtV-fzlzRhVLhK&index=4&pp=iAQB'
 	},
 	{
 		filmname: '番外02',
 		cover: '../imgs/cover/extra02.png',
-		url: 'https://www.bilibili.com/video/BV1XpmgBNECb/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1XpmgBNECb/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=oa8_til4wgI&list=PLmXJJ8Www-ajXcF38YnKtV-fzlzRhVLhK&index=3&pp=iAQB'
 	},
 	{
 		filmname: '番外03',
 		cover: '../imgs/cover/extra03.jpg',
-		url: 'https://www.bilibili.com/video/BV1HQAFzbEH8/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1HQAFzbEH8/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=FbqQoFTfYiQ&list=PLmXJJ8Www-ajXcF38YnKtV-fzlzRhVLhK&index=2&pp=iAQB'
 	},
 	{
 		filmname: '番外04',
 		cover: '../imgs/cover/extra04.png',
-		url: 'https://www.bilibili.com/video/BV1s2Ts6YEee/?spm_id_from=333.1387.collection.video_card.click'
+		url: 'https://www.bilibili.com/video/BV1s2Ts6YEee/?spm_id_from=333.1387.collection.video_card.click',
+		yt: 'https://www.youtube.com/watch?v=JHywkDyDPoU&list=PLmXJJ8Www-ajXcF38YnKtV-fzlzRhVLhK&index=1&pp=iAQB'
 	}
 ]
